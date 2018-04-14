230334a603e14458ca630ca7df5212b4b487fae9
hanashiryuta <kbrn.kabirunrun@gmail.com>
2018-04-14 17:58:29 +0900

    金メッキ
    剥げてるに変更

--- UnityTEST/Assets/test.cs
+++ UnityTEST/Assets/test.cs
@@ -6,7 +6,7 @@ public class test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.log("はげてない！");
+        Debug.log("剥げてる！");
 	}
 	
 	// Update is called once per frame
