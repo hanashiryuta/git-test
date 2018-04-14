32fcd4915c6a3ae20f0b6f8568f3462862cf96e6
hanashiryuta <kbrn.kabirunrun@gmail.com>
2018-04-14 15:10:30 +0900

    test竜太
    葉梨を竜太に変更

--- UnityTEST/Assets/test.cs
+++ UnityTEST/Assets/test.cs
@@ -6,7 +6,7 @@ public class test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.log("葉梨！");
+        Debug.log("竜太！");
 	}
 	
 	// Update is called once per frame
