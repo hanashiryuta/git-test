aff7deddb9d3b185bff32cb18bdd9aeacccaed94
hanashiryuta <kbrn.kabirunrun@gmail.com>
2018-04-14 15:13:09 +0900

    禿
    禿に変更

--- UnityTEST/Assets/test.cs
+++ UnityTEST/Assets/test.cs
@@ -6,7 +6,7 @@ public class test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.log("竜太！");
+        Debug.log("禿！");
 	}
 	
 	// Update is called once per frame
